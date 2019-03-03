b005d9f3897ac0dacf88bf65ef5d519118387ec0
joyzhengyz <yezhenzheng@gmail.com>
2019-03-02 21:47:28 -0500

    Update 162findPeak.py

--- 162findPeak.py
+++ 162findPeak.py
@@ -6,16 +6,18 @@ class Solution(object):
         """
         l, r = 0, len(nums) - 1
         while l < r:
-            mid = l + (r - l) // 2
-            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
-                return mid
-            elif nums[mid] < nums[mid + 1]:
-                l = mid + 1
+            m = l + (r - l) // 2
+            print "l, r, mid ", l, r, mid
+            if nums[m] > nums[m + 1] and nums[m] > nums[m - 1]:
+                return m
+            elif nums[m] < nums[m + 1]:
+                l = m + 1
             else:
-                r = mid
+                r = m
         return l
 
 
 if __name__ == "__main__":
     a = [1,2,3,1]
+    # a = [1,9,4,4,4,2,1]
     print Solution().findPeakElement(a)
