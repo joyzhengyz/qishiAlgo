bcd666a3bc112b7c695e66d8d314de8d53a93a3b
joyzhengyz <yezhenzheng@gmail.com>
2019-03-03 00:16:07 -0500

    Update gasStation.py

--- gasStation.py
+++ gasStation.py
@@ -8,13 +8,11 @@ class Solution(object):
         """
         #0.000001 == 1e-6
         left, right = 1e-6, stations[-1] - stations[0]
-
-        while left + 1e-6 < right:
+        while left < right:
             mid = left + (right - left) / 2
             count = 0
             for a, b in zip(stations, stations[1:]):
                 count += math.ceil((b - a) / mid) - 1
-                print a, b, count, math.ceil((b - a) / mid)
             if count > k:
                 left = mid
             else:
